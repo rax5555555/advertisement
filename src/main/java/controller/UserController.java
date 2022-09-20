b93cae7a132a8299e8a61fca52118443a440f999
Timur Rakhmatullin <rax_5@mail.ru>
2022-09-20 19:00:37 +0500

    Add User

---
+++ src/main/java/controller/UserController.java
@@ -0,0 +1,53 @@
+package controller;
+
+
+import dto.UserDto;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import servise.UserService;
+
+import java.util.List;
+
+
+@Slf4j
+@RestController
+@RequestMapping(path = "/users")
+public class UserController {
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    UserDto create(@RequestBody UserDto userDto) {
+        log.info("create user");
+        return userService.create(userDto);
+    }
+
+    @PatchMapping("/{userId}")
+    public UserDto update(@PathVariable Long userId, @RequestBody UserDto userDto) {
+        log.info("update user id ={}", userId);
+        return userService.update(userId, userDto);
+    }
+
+    @DeleteMapping("/{userId}")
+    public void delete(@PathVariable Long userId) {
+        log.info("delete user id={}", userId);
+        userService.delete(userId);
+    }
+
+    @GetMapping("/{userId}")
+    public UserDto getUserById(@PathVariable Long userId) {
+        log.info("get user id={}", userId);
+        return userService.getUserById(userId);
+    }
+
+    @GetMapping
+    public List<UserDto> getAllUsers() {
+        log.info("get all users");
+        return userService.getAllUsers();
+    }
+}
